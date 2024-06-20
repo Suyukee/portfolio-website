7f2d897d50ae5ace8cbab03f77a5a7c719defb0a
Suyukee <diefuck@ya.ru>
2024-06-20 16:17:55 +0300

    Refactoring

--- src/components/project-section.jsx
+++ src/components/project-section.jsx
@@ -7,9 +7,19 @@ export default function ProjectSection() {
 			<div className={styles.project}>
 				<h2>Мои проекты</h2>
 				<div className={styles.filter}>
-					<FilterButton active={true}>Все</FilterButton>
+					{/* <FilterButton active={true}>Все</FilterButton>
 					<FilterButton active={false}>TypeScript</FilterButton>
-					<FilterButton active={false}>Мобильные</FilterButton>
+					<FilterButton active={false}>Мобильные</FilterButton> */}
+
+					<button className={`${styles.button} ${styles.active}`}>
+						<h3>Все</h3>
+					</button>
+					<button className={styles.button}>
+						<h3>TypeScript</h3>
+					</button>
+					<button className={styles.button}>
+						<h3>Мобильные</h3>
+					</button>
 				</div>
 			</div>
 		</section>
