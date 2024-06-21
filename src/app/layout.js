d9badcffd04e38ff11bc563da61c086352b2e94f
Suyukee <diefuck@ya.ru>
2024-06-21 15:03:20 +0300

    Refactoring

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,22 +8,9 @@ export const metadata = {
 	description: 'Portfolio website',
 };
 
-export const viewport = {
-	themeColor: [
-		{ media: '(prefers-color-scheme: light)', color: 'red' },
-		{ media: '(prefers-color-scheme: dark)', color: 'green' },
-	],
-	colorScheme: 'light',
-};
-
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<head>
-				<meta name="theme-color" media="(prefers-color-scheme: light)" content="red" />
-				<meta name="theme-color" media="(prefers-color-scheme: dark)" content="green" />
-				<meta name="color-scheme" content="light" />
-			</head>
 			<body className={comicSans.className}>{children}</body>
 		</html>
 	);
