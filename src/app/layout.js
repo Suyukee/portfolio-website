799f48bb81f9e76554502a5d72a700e9489cfba0
Suyukee <diefuck@ya.ru>
2024-06-21 14:59:56 +0300

    testing viewport

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,14 +10,20 @@ export const metadata = {
 
 export const viewport = {
 	themeColor: [
-		{ media: '(prefers-color-scheme: light)', color: '#f8f8f8' },
-		{ media: '(prefers-color-scheme: dark)', color: '#212529' },
+		{ media: '(prefers-color-scheme: light)', color: 'red' },
+		{ media: '(prefers-color-scheme: dark)', color: 'green' },
 	],
+	colorScheme: 'light',
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<meta name="theme-color" media="(prefers-color-scheme: light)" content="red" />
+				<meta name="theme-color" media="(prefers-color-scheme: dark)" content="green" />
+				<meta name="color-scheme" content="light" />
+			</head>
 			<body className={comicSans.className}>{children}</body>
 		</html>
 	);
