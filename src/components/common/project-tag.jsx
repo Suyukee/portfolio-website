dfafb8b540282c9a495e614d6b9348f3a86fd6a3
Suyukee <diefuck@ya.ru>
2024-06-24 18:09:18 +0300

    Refactoring

--- src/components/common/project-tag.jsx
+++ src/components/common/project-tag.jsx
@@ -2,11 +2,11 @@ import styles from '../../styles/project-tag.module.css';
 
 export default function ProjectTag({ name, onClick, isSelected }) {
 	return (
-		<button
+		<div
 			className={`${styles.button} ${isSelected && styles.active}`}
 			onClick={() => onClick(name)}
 		>
 			<h3>{name}</h3>
-		</button>
+		</div>
 	);
 }
