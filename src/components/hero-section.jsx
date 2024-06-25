ed85154c2b7537f74c5c29338eaef679051eabdf
Suyukee <diefuck@ya.ru>
2024-06-25 14:30:54 +0300

    Added link for avatar

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from '../styles/hero-section.module.css';
 import EditingBorder from './common/editing-border';
+import Link from 'next/link';
 
 export default function HeroSection() {
 	return (
@@ -10,11 +11,11 @@ export default function HeroSection() {
 					<h1>Привет, я Вова</h1>
 					<h2>frontend-разработчик</h2>
 				</div>
-				<div className={styles.avatar}>
+				<Link className={styles.avatar} href="https://github.com/Suyukee">
 					<EditingBorder>
 						<Image src="/images/avatar.jpg" width={300} height={300} alt="Фото" priority={true} />
 					</EditingBorder>
-				</div>
+				</Link>
 			</div>
 		</section>
 	);
