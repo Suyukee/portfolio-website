b62cfae2acbe8852aecd62b4cc3325b2bf9ff5f9
Suyukee <diefuck@ya.ru>
2024-06-26 18:02:57 +0300

    Added links on contacts

--- src/components/common/project-tag.jsx
+++ src/components/common/project-tag.jsx
@@ -2,11 +2,8 @@ import styles from '../../styles/project-tag.module.css';
 
 export default function ProjectTag({ name, onClick, isSelected }) {
 	return (
-		<div
-			className={`${styles.button} ${isSelected && styles.active}`}
-			onClick={() => onClick(name)}
-		>
+		<button className={`button ${isSelected && styles.active}`} onClick={() => onClick(name)}>
 			<h3>{name}</h3>
-		</div>
+		</button>
 	);
 }
--- src/components/stack-section.jsx
+++ src/components/stack-section.jsx
@@ -3,11 +3,16 @@ import styles from '../styles/stack-section.module.css';
 function StackSection() {
 	return (
 		<section className="section">
-			<div className={styles.section__stack}>
-				<h3>JavaScript</h3>
-				<h3>TypeScript</h3>
-				<h3>React</h3>
-				<h3>Next.js</h3>
+			<div className={styles.stack}>
+				<div className={styles.technologies}>
+					<h3>JavaScript</h3>
+					<h3>TypeScript</h3>
+					<h3>React</h3>
+					<h3>Next.js</h3>
+				</div>
+				{/* <h3>Оконченное высшее в IT сфере</h3>
+				<h3>Пройденный курс TypeScript + React</h3>
+				<h3>Больше года обучения</h3> */}
 			</div>
 		</section>
 	);
